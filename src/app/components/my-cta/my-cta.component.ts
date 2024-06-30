fa6522135ad312c24cbeb52a1f80f875e98d1323
piz <begonzo82@gmail.com>
2024-06-30 12:19:58 +0200

    aggiunta la navigazione da pulsante del footer; varie

--- src/app/components/my-cta/my-cta.component.ts
+++ src/app/components/my-cta/my-cta.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'my-cta',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './my-cta.component.html',
   styleUrl: './my-cta.component.css'
 })
 export class MyCtaComponent {
-
+  @Input() text: string = '';
+  @Input() href: string = '';
+  @Input() linkText: string = '';
 }
